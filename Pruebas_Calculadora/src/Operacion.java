48406a5caf714e7c32275cfda2cae2f3b16d4d14
@ñarly <carrabmin@gmail.com>
2021-02-11 16:14:05 +0100

    Primer commit de la clase Operacion

---
+++ Pruebas_Calculadora/src/Operacion.java
@@ -0,0 +1,22 @@
+/**
+ * Esta es una clase del proyecto Calculadora que realiza operaciones
+ * 
+ * @author Carlos Rabinal Mínguez
+ * @version 1.0
+ * @since 30/01/2021
+ * 
+ * CASO ESPECIAL: Si la calculadora no pudiera utilizar números negativos,
+ * mostraremos un mensaje por pantalla para advertir que sólo podemos introducir números positivos
+ *
+ * 
+ */
+public class Operacion {
+	
+/**
+ * Este es un método constructor vacío (sin parámetros)
+ */
+	public Operacion() {
+		super();
+	}
+
+}
