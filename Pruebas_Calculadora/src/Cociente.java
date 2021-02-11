5d008b688534bde60c1a040caf61e93ac6700770
@ñarly <carrabmin@gmail.com>
2021-02-11 18:56:24 +0100

    Primer commit - Comentarios iniciales sobre la clase Cociente

---
+++ Pruebas_Calculadora/src/Cociente.java
@@ -0,0 +1,15 @@
+/**
+ * Clase Cociente, ejercicio grupal 1
+ * @author Juank
+ *@version 1.0
+ *@since 01/02/2021
+ *
+ * CASO ESPECIAL: Si nos introducen números negativos, mostraremos un mensaje por pantalla 
+ * indicando que sólo funciona con números positivos
+ *  
+ * CASO ESPECIAL: Si la división devuelve un resto diferente a 0, significa que esta no es exacta.
+ */ 
+
+public class Cociente {
+
+}
