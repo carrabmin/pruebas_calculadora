ce3545bea39d129b3ad5aff525301c7a866a40d5
@ñarly <carrabmin@gmail.com>
2021-02-11 16:45:35 +0100

    Segundo commit de la clase Operacion - Metodo esPrimo

--- Pruebas_Calculadora/src/Operacion.java
+++ Pruebas_Calculadora/src/Operacion.java
@@ -18,5 +18,24 @@ public class Operacion {
 	public Operacion() {
 		super();
 	}
+	
+/**
+ * Este es el método booleano para calcular si un número es primo o no
+ * 
+ * @param n es el parámetro que introducimos para averiguar si dicho número es primo o no
+ * @return true si el número es primo y false si no es número primo
+ * 
+ *  CASO ESPECIAL: Si introducimos un cero como parámetro, el método nos devuelve false
+ */
+	public boolean esPrimo(int n){
+	    int a = 0;
+	        for (int i = 1; i < (n + 1); i++) 
+	            if (n % i == 0) 
+	                a++;
+	        if (a != 2) 
+	           return false;
+	         else 
+	          return true;
+    }
 
 }
